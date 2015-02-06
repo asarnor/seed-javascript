1a7a9dd636f459c95d8e908ceb1d0c33c386be7a
asarnor <teamcodebehind@hotmail.com>
2015-02-05 23:01:28 -0500

    created unit tests for functions in prime palindrome application

--- spec/ApplicationSpec.js
+++ spec/ApplicationSpec.js
@@ -1,9 +1,25 @@
 describe("Application", function() {
     var application = new Application();
 
-    it("should do what I specify", function() {
+    it("should determine if the input is prime", function() {
 
-      expect( null ).toBe( application.myFunction() );
+        expect(false).toBe(application.isPrime(999));
+
+        expect(true).toBe(application.isPrime(929));
+
+    });
+
+    it("should determine if the input is Palindrome", function() {
+
+        expect(false).toBe(application.isPalindrome(9990));
+
+        expect(true).toBe(application.isPalindrome(929));
+
+    });
+
+    it("should return the largest prime palindrome less than 1000", function() {
+
+        expect(929).toBe(application.primePalindrome());
 
     });
 });
